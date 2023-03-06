189fe039fbdf9ef667d745e48880e83548a35f1d
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-03-06 13:31:51 +0100

    Initial commit

---
+++ ParkHere/ParkHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkHereApp.swift
+//  ParkHere
+//
+//  Created by Jakub Prus on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParkHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
