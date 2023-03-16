522a2765ea440f9e8c7f36a8609ecb0bbbdf3369
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-03-16 01:18:25 +0100

    new: parking preview struct

---
+++ ParkHere/Resources/Structs/ParkingPreviewStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ParkingPreviewStruct.swift
+//  ParkHere
+//
+//  Created by Jakub Prus on 16/03/2023.
+//
+
+import Foundation
+
+struct ParkingPreview: Codable {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
