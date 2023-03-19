74a0cb3a45f3b05d55f4025e23543e5f140101d7
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-03-19 01:11:29 +0100

    feat: back arrow function and new SearchBarView file

---
+++ ParkHere/Views/Components/SearchBar/SearchBarView.swift
@@ -0,0 +1,62 @@
+//
+//  SearchBarView.swift
+//  ParkHere
+//
+//  Created by Jakub Prus on 18/03/2023.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchTerm: String
+    @FocusState var isFocused: Bool
+    @Binding var isSearching: Bool
+    
+    var body: some View {
+        HStack {
+            if isSearching{
+                Image(systemName: "arrow.backward")
+                    .onTapGesture {
+                        isFocused = false
+                        isSearching = false
+                        searchTerm = ""
+                    }
+            }
+            else {
+                Image("search")
+                    .flatIconImage()
+                    .onTapGesture {
+                        isSearching = false
+                        isFocused = false
+                    }
+            }
+            
+            TextField("Search location", text: $searchTerm)
+                .focused($isFocused)
+                .onChange(of: isFocused) { _ in
+                    isSearching = isFocused
+                }
+            
+            Image("filter")
+                .flatIconImage()
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .fontWeight(.semibold)
+        .background(.white)
+        .cornerRadius(20)
+        .shadow(radius: 1)
+        .onChange(of: isSearching) { _ in
+            isFocused = isSearching
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    @State static var searchTerm = ""
+    @State static var isSearching = false
+    static var previews: some View {
+        SearchBarView(searchTerm: $searchTerm, isSearching: $isSearching)
+    }
+}
