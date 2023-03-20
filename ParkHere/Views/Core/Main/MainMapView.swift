167daf95d8fbad43c79b3759094eecc55fd221f6
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-03-20 01:35:04 +0100

    feat: detail views

--- ParkHere/Views/Core/Main/MainMapView.swift
+++ ParkHere/Views/Core/Main/MainMapView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 import MapKit
 
-
 struct MainMapView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.23,longitude: 21.0),span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
     @State private var searchTerm: String = ""
     @State private var isSearching: Bool = false
     @StateObject var allParkings = ParkingFirestoreManager()
+    @State private var showParkingPreview = false
+    @State private var currentParking = ParkingStruct.sampleParking
     
     @Binding var selectedTab: Tab
     
@@ -23,9 +24,28 @@ struct MainMapView: View {
             ZStack(alignment: .top) {
                 // MARK: MAP
                 Map(coordinateRegion: $mapRegion, annotationItems: allParkings.firestoreParkings){ parking in
-                    MapMarker(coordinate: CLLocationCoordinate2D(
+//                    MapMarker(coordinate: CLLocationCoordinate2D(
+//                        latitude: parking.location.latitude,
+//                        longitude: parking.location.longitude))
+                    
+                    // MapAnnotation causes a bug!!!
+                    // But MapMarker does not
+                    // soruce: https://developer.apple.com/forums/thread/718697
+                    
+                    MapAnnotation(coordinate: CLLocationCoordinate2D(
                         latitude: parking.location.latitude,
                         longitude: parking.location.longitude))
+                    {
+                            VStack{
+                                Image(systemName: "parkingsign.circle.fill")
+                                    .font(.title)
+                                    .foregroundColor(.blue)
+                            }
+                            .onTapGesture {
+                                self.currentParking = parking
+                                self.showParkingPreview = true
+                            }
+                        }
                 }
                 .ignoresSafeArea()
                 .opacity(isSearching ? 0.01 : 1 )
@@ -34,7 +54,7 @@ struct MainMapView: View {
                     isSearching = false
                     searchTerm = ""
                 }
-                
+                // MARK: Top Safe Area
                 GeometryReader { reader in
                     Color.clear
                         .background(.ultraThinMaterial)
@@ -42,8 +62,11 @@ struct MainMapView: View {
                         .ignoresSafeArea()
                 }
                 VStack {
+                    
+                    // Searchbar view component
                     SearchBarView(searchTerm: $searchTerm, isSearching: $isSearching)
                     
+                    // White view with results
                     if isSearching {
                         searchView
                             .opacity(isSearching ? 1 : 0.01 )
@@ -52,6 +75,12 @@ struct MainMapView: View {
                     }
                     Spacer()
                     
+                    // Bottom sheet with preview info
+                    if showParkingPreview && !isSearching {
+                        ParkingDetailsPreview(parking: currentParking, isShowingPreview: $showParkingPreview)
+                            .padding(.bottom, 50)
+                            .animation(.linear(duration: 0.5), value: showParkingPreview)
+                    }
                 } // VStack
                 .padding()
                 
@@ -80,9 +109,19 @@ extension MainMapView {
                 .frame(maxWidth: .infinity, alignment: .leading)
             
             ForEach(searchResults) { parking in
-                // TODO: Present new view after click
-                ParkingResultSearchView(parking: parking)
-                    .padding(.top, 10)
+                Button{
+                    isSearching = false
+                    showParkingPreview = true
+                    currentParking = parking
+                    mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: parking.location.latitude, longitude: parking.location.longitude),span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                } label: {
+                    // Single result view component
+                    ParkingResultSearchView(parking: parking)
+                        .padding(.top, 10)
+                        .foregroundColor(.black)
+                }
+                
+                
                 
             }
         }
