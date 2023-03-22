6515c8397a1f25c93109da8ba04755079545d793
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-03-22 09:35:46 +0100

    add: dismiss by cancel

--- ParkHere/Views/Components/Parking/ParkingDetailsView.swift
+++ ParkHere/Views/Components/Parking/ParkingDetailsView.swift
@@ -13,6 +13,7 @@ struct ParkingDetailsView: View {
     let parking: ParkingStruct
     @State var parkings = [ParkingStruct]()
     @State var mapRegion = MKCoordinateRegion()
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         NavigationStack {
@@ -35,7 +36,6 @@ struct ParkingDetailsView: View {
                                     .font(.callout)
                                 
                             }
-                            //.padding(.vertical)
                             
                             // Non-interactive map
                             Map(coordinateRegion: $mapRegion, annotationItems: parkings){parking in
@@ -103,13 +103,13 @@ struct ParkingDetailsView: View {
                     
                     // MARK: Buttons
                     VStack{
-                        Button("Reserve your parking spot"){
-                            // TODO: Callendar view with reservation
+                        NavigationLink("Reserve your parking spot"){
+                            CalendarView(parking: parking)
                         }
                         .buttonStyle(.sign)
                         .padding()
                         Button("Cancel"){
-                            // back
+                            dismiss()
                         }
                         .foregroundColor(.gray)
                         .padding(.top, -10)
